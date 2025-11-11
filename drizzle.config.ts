e5c1a441663ec383ceaaa94451974518019ffa8d
miguel <miguelangel.alarcos@proton.me>
2025-11-11 18:56:33 +0100

    wip

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'drizzle-kit';
 
+import { config } from "dotenv";
+
+config({ path: ".env.local" });
 if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 export default defineConfig({
